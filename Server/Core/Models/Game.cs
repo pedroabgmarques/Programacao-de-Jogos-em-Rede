a7b07e41f62c193c7907f1fae9d654f2f980bfa6
pedroabgmarques <pedroabgmarques@gmail.com>
2016-09-22 14:56:15 +0100

    Classes Game (estado do jogo) e Move (descreve uma jogada)

---
+++ Server/Core/Models/Game.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Models
+{
+
+    /// <summary>
+    /// Guarda o estado do jogo
+    /// </summary>
+    public class Game
+    {
+        /// <summary>
+        /// Lista de jogadores presentes no jogo
+        /// </summary>
+        public List<Player> ListaJogadores { get; set; }
+
+        /// <summary>
+        /// Numero que os jogadores tentam adivinhar
+        /// </summary>
+        public int TargetNumber { get; set; }
+
+        /// <summary>
+        /// Lista de jogadas deste jogo
+        /// </summary>
+        public List<Move> ListaJogadas { get; set; }
+
+    }
+}
---
+++ Server/Core/Models/Move.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Models
+{
+
+    /// <summary>
+    /// Descreve uma jogada
+    /// </summary>
+    public class Move
+    {
+        /// <summary>
+        /// Identifica de forma unica uma jogada
+        /// </summary>
+        public int ID { get; set; }
+
+        /// <summary>
+        /// Jogador que efetuou a jogada
+        /// </summary>
+        public Player jogador { get; set; }
+
+        /// <summary>
+        /// Data e hora em que a jogada foi efetuada
+        /// </summary>
+        public DateTime DateTimePlay { get; set; }
+
+        /// <summary>
+        /// Numero que o jogador tentou adivinhar
+        /// </summary>
+        public int PlayedNumber { get; set; }
+    }
+}
