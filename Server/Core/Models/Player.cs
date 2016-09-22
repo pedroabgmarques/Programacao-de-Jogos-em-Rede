71d9c092bc3ad1ae06b7cc60e17aafac94dc62aa
pedroabgmarques <pedroabgmarques@gmail.com>
2016-09-22 14:50:04 +0100

    Propriedades da classe Player

--- Server/Core/Models/Player.cs
+++ Server/Core/Models/Player.cs
@@ -12,5 +12,20 @@ namespace Core.Models
     /// </summary>
     public class Player
     {
+        /// <summary>
+        /// Identifica o jogador de forma única
+        /// </summary>
+        public int ID { get; set; }
+
+        /// <summary>
+        /// Nome do jogador
+        /// </summary>
+        public int Nome { get; set; }
+
+        /// <summary>
+        /// Endereço IP do jogador
+        /// </summary>
+        public int IPAddress { get; set; }
+        
     }
 }
