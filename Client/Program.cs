78543a0c8c35084d66186d0a65401405606aeaa6
pedroabgmarques <pedroabgmarques@gmail.com>
2016-09-22 14:35:46 +0100

    Projeto Client vazio

--- Client/Program.cs
+++ Client/Program.cs
@@ -4,15 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Server
+namespace Client
 {
     class Program
     {
         static void Main(string[] args)
         {
-
-
-
         }
     }
 }
