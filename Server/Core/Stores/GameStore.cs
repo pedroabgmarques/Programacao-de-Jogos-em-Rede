2bb3dde28f8f5f167f64db4857773b199fc10c12
pedroabgmarques <pedroabgmarques@gmail.com>
2016-09-22 15:08:56 +0100

    Singleton GameStore

---
+++ Server/Core/Stores/GameStore.cs
@@ -0,0 +1,38 @@
+﻿using Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Stores
+{
+
+    /// <summary>
+    /// Singleton para disponibilizar uma e apenas uma instância de Game
+    /// </summary>
+    public sealed class GameStore
+    {
+        private static GameStore _instance;
+
+        public Game Game { get; set; }
+
+        private GameStore()
+        {
+            Game = new Game();
+        }
+
+        public static GameStore Instance()
+        {
+            if (_instance != null)
+            {
+                return _instance;
+            }
+            else
+            {
+                return new GameStore();
+            }
+        }
+        
+    }
+}
